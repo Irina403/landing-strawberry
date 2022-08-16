f19d2f65e720ac0ba0622f9c1d2135dcb020ec53
Irina403 <92080770+Irina403@users.noreply.github.com>
2022-08-16 10:59:51 +0300

    start 16.08

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,22 @@
-
+$('.js-open-modal').click(function(event) {
+    event.preventDefault();
+    
+    var modalName = $(this).attr('data-modal');
+    var modal = $('.modal[data-modal="' + modalName + '"]');
+    
+    modal.addClass('is-show');
+    $('.js-modal-overlay').addClass('is-show');
+ });
+ 
+ $('.modal__close').click(function() {
+    $(this).parent('.modal').removeClass('is-show');
+    $('.js-modal-overlay').removeClass('is-show');
+ });
+    
+ $('.js-modal-overlay').click(function() {
+    $('.modal.is-show').removeClass('is-show');
+    $(this).removeClass('is-show');
+ });
 
 
 
