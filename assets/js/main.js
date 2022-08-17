e31ad05cac37f1c5d88e76c92b3b075768ec1948
Irina403 <92080770+Irina403@users.noreply.github.com>
2022-08-17 17:20:30 +0300

    17:20

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,41 @@ $('.js-open-modal').click(function(event) {
         $(this).removeClass('is-show');
     });
 
+
+$('.order').click(function(event) {
+    event.preventDefault();
+
+    var modal = $('.modal-form');
+    modal.addClass('is-show');
+
+    // document.querySelector('.modal').style.display = 'none';
+    
+    $('.js-modal-overlay').addClass('is-show');
+    });
+
+    $('.modal-form__close').click(function() {
+        $(this).parent('.modal-form').removeClass('is-show');
+        $('.js-modal-overlay').removeClass('is-show');
+    });
+        
+    $('.js-modal-overlay').click(function() {
+        $('.modal-form.is-show').removeClass('is-show');
+        $(this).removeClass('is-show');
+    });
+
+    // if('modal').classList.conntains('is-show'){
+    //     console.log('yes');
+    // };
+    $('.order').click(function(event) {
+        if($('.modal').hasClass('is-show')){
+            $('.modal').removeClass('is-show');
+            // alert('У этого блока есть класс test1');
+        }else{
+            alert('У этого блока нет класса test1');
+          }
+      });
+        
+
     $(document).ready(function(){
         $('.modal__slide ').slick({
             dots: true,
